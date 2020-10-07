29c688e7dbdf84a4a8e44076cc83703e92c14b07
TeddyBear2004 <thorgeengelmann@gmail.com>
2020-10-07 11:14:20 +0200

    added language config to ReloadCommand

--- src/main/java/de/dreamtale/teddy/twitch/chat/minecraft/event/ReloadCommand.java
+++ src/main/java/de/dreamtale/teddy/twitch/chat/minecraft/event/ReloadCommand.java
@@ -4,14 +4,13 @@ import de.dreamtale.teddy.twitch.chat.Main;
 import net.minecraftforge.client.event.ClientChatEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-import java.util.Objects;
-
 public class ReloadCommand {
     @SubscribeEvent
     public void onSendEvent(ClientChatEvent event){
         if(event.getMessage().toLowerCase().startsWith("#reload".toLowerCase())){
             event.setCanceled(true);
             Main.twitchChatConfig.reload();
+            Main.languageConfig.reload();
 
             String message = Main.languageConfig.getString("reload_success");
 
